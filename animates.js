4e67d9041dfa77d4c344a1a61131153db5bc14a5
Wildmonk <114974010+WildmonkGitHub@users.noreply.github.com>
2024-05-08 22:34:33 +0530

    Add files via upload

--- animates.js
+++ animates.js
@@ -1,4 +1,5 @@
 const lion = document.getElementById("logo-w");
+const opt = document.getElementById("opt1")
 
 function changePhoto() {
     lion.src = "./lib/redlion.png";
@@ -7,3 +8,9 @@ function changePhoto() {
 function resetphoto() {
     lion.src = "./lib/lion.jpeg";
 }
+
+function displaymedia() {
+    opt.style = "transition: 0.5s; margin-left: 0px;"
+}
+
+document.onclick = function () {opt.style = "transition: 0.5s; margin-left: -900px;"}
