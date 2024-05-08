b7758282dfe4336619de48e9ba35d933c18c04b9
Wildmonk <114974010+WildmonkGitHub@users.noreply.github.com>
2024-05-08 22:46:29 +0530

    View our new "update page"!

--- animates.js
+++ animates.js
@@ -1,5 +1,6 @@
 const lion = document.getElementById("logo-w");
 const opt = document.getElementById("opt1")
+let opener = 0;
 
 function changePhoto() {
     lion.src = "./lib/redlion.png";
@@ -9,8 +10,13 @@ function resetphoto() {
     lion.src = "./lib/lion.jpeg";
 }
 
-function displaymedia() {
-    opt.style = "transition: 0.5s; margin-left: 0px;"
-}
 
-document.onclick = function () {opt.style = "transition: 0.5s; margin-left: -900px;"}
+function showmedia() {
+    if (opener == 0) {
+        opt.style = "transition: 0.5s; margin-left: 0px;"
+        opener = 1
+    } else {
+        opener = 0
+        opt.style = "transition: 0.5s; margin-left: -900px;"
+    }
+}
