cf5a9e04ea5643c309797b19a841733290486ea4
Wildmonk <114974010+WildmonkGitHub@users.noreply.github.com>
2024-05-18 22:41:11 +0530

    Added Navbar

--- assembler.js
+++ assembler.js
@@ -15,4 +15,41 @@ function age() {
     return Age
 }
 
+var prevScrollpos = 42;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > prevScrollpos) {
+    document.getElementById("b2").style.position = "fixed"
+    document.getElementById("b2").style.top = "0px";
+    document.getElementById("projectsmenu").style.position = "fixed"
+    document.getElementById("projectsmenu").style.top = "45px";
+    document.getElementById("settingsmenu").style.position = "fixed"
+    document.getElementById("settingsmenu").style.top = "45px";
+  } else {
+    document.getElementById("b2").style.position = "absolute"
+    document.getElementById("b2").style.top = "40px";
+    document.getElementById("projectsmenu").style.position = "absolute"
+    document.getElementById("projectsmenu").style.top = "90px";
+    document.getElementById("settingsmenu").style.position = "absolute"
+    document.getElementById("settingsmenu").style.top = "90px";
+  }
+}
+
 document.getElementById("age").innerHTML = age()
+const al = document.getElementById("projectsmenu")
+
+function applyProject() {
+    if (al.style.display == "none") {
+        al.style.display = "block";
+        sl.style.display = "none";
+    } else {al.style.display = "none";}
+}
+
+const sl = document.getElementById("settingsmenu")
+
+function applySettings() {
+    if (sl.style.display == "none") {
+        sl.style.display = "block";
+        al.style.display = "none";
+    } else {sl.style.display = "none";}
+}
