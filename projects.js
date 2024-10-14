29aedd41d0c3b2c26381ca9db7c4e06d9a5e01bd
Wildmonk <114974010+WildmonkGitHub@users.noreply.github.com>
2024-10-14 22:26:16 +0530

    Updated

--- projects.js
+++ projects.js
@@ -3,6 +3,7 @@
 
 const projects = document.getElementById("projects");
 const videos = document.getElementById("videos");
+const videosAB = document.getElementById("videos-a");
 
 function addProjects(link, imglink, text) {
     let i;
@@ -25,13 +26,13 @@ function addProjects(link, imglink, text) {
     s.appendChild(t);
 }
 
-addProjects("https://scratch.mit.edu/projects/984298414/", "https://uploads.scratch.mit.edu/get_image/project/984298414_846x630.png", "YouTube Tutorials be like");
+addProjects("https://scratch.mit.edu/projects/1077545562/", "https://uploads.scratch.mit.edu/get_image/project/1077545562_440x330.png", "Pico and the Suspicious Stew");
 
-addProjects("https://scratch.mit.edu/projects/853243007/", "https://uploads.scratch.mit.edu/get_image/project/853243007_440x330.png", "Nano dumb jokes");
+addProjects("https://scratch.mit.edu/projects/670871895/", "https://uploads.scratch.mit.edu/get_image/project/670871895_432x324.png", "Pico's Grammar Mistake");
 
-addProjects("https://scratch.mit.edu/projects/839503951/", "https://uploads.scratch.mit.edu/get_image/project/839503951_440x330.png", "Mr. Cursor");
+addProjects("https://scratch.mit.edu/projects/729027411/", "https://uploads.scratch.mit.edu/get_image/project/729027411_432x324.png", "What happens when MouseHeadz get...");
 
-addProjects("https://scratch.mit.edu/projects/839505254/", "https://uploads.scratch.mit.edu/get_image/project/839505254_440x330.png", "MouseHeadz but no glasses");
+addProjects("https://scratch.mit.edu/projects/863918076/", "https://uploads.scratch.mit.edu/get_image/project/863918076_432x324.png", "Scratch Minecraft Survival Series");
 
 function addVideos(link, imglink, text) {
     let i;
@@ -54,10 +55,33 @@ function addVideos(link, imglink, text) {
     s.appendChild(t);
 }
 
-addVideos("https://www.youtube.com/watch?v=ARIf3Av47qU", "https://uploads.scratch.mit.edu/get_image/project/839376992_440x330.png", "Once upon a time...")
+addVideos("https://www.youtube.com/watch?v=bVlyHaT_e_Q", "https://uploads.scratch.mit.edu/get_image/project/1060606026_440x330.png", "Scratch Cat and his talking tail")
 
-addVideos("https://www.youtube.com/watch?v=gjdNbsobPyU", "https://uploads.scratch.mit.edu/get_image/project/785044441_432x324.png", "Edward lose in Tic Tac Toe")
+addVideos("https://www.youtube.com/watch?v=IwnzoGWs4Mg", "https://uploads.scratch.mit.edu/get_image/project/1077547209_432x324.png", "What if MouseHeadz drank WRONG POTI...")
 
-addVideos("https://www.youtube.com/watch?v=l2TkF-MMZ7s", "https://uploads.scratch.mit.edu/get_image/project/809681205_432x324.png", "Edward and Oliver Game Review")
+addVideos("https://www.youtube.com/watch?v=_7jjEw6visU", "https://uploads.scratch.mit.edu/get_image/project/1077545295_440x330.png", "Edward teaches Kemistree!?")
 
-addVideos("https://www.youtube.com/watch?v=LSq1euT_DfU", "https://uploads.scratch.mit.edu/get_image/project/739621967_432x324.png", "If MouseHeadz were glasses")
+addVideos("https://www.youtube.com/watch?v=xsGDyLXURJg", "https://uploads.scratch.mit.edu/get_image/project/642767709_432x324.png", "What happens when Math hater...")
+
+function addVideosA(link, imglink, text) {
+    let i;
+    let s;
+    let t;
+    s = document.createElement("span")
+    i = document.createElement("img")
+    t = document.createElement("p")
+    i.className = "youtubevideo"
+    i.height = 200;
+    i.title = text;
+    i.onclick = function() {document.location = link;};
+    i.src = imglink;
+    t.style = "font-weight: 800;";
+    t.innerHTML = text;
+    s.style = "display: inline-block; margin-right: 20px;"
+
+    videosAB.appendChild(s);
+    s.appendChild(i);
+    s.appendChild(t);
+}
+
+addVideosA("https://www.youtube.com/watch?v=1dy-blqvGWE", "https://uploads.scratch.mit.edu/get_image/project/1059875039_846x630.png", "Gobo got an ENDER PEARL")
